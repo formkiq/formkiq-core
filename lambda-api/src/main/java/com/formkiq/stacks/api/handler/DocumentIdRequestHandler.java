293bf648f387c7dca3cdfcc5dfaaafd14c3e1432
Mike Friesen <mfriesen@gmail.com>
2020-11-28 19:15:25 -0600

    Fixed POST /documents setting the correct 'Content-Type' when Content-Header is set.

--- lambda-api/src/main/java/com/formkiq/stacks/api/handler/DocumentIdRequestHandler.java
+++ lambda-api/src/main/java/com/formkiq/stacks/api/handler/DocumentIdRequestHandler.java
@@ -3,23 +3,20 @@
  * 
  * Copyright (c) 2018 - 2020 FormKiQ
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package com.formkiq.stacks.api.handler;
 
@@ -277,6 +274,7 @@ public class DocumentIdRequestHandler
     String maxDocumentCount = null;
 
     DynamicObject item = fromBodyToDynamicObject(logger, event);
+    updateContentType(event, item);
 
     List<DynamicObject> documents = item.getList("documents");
 
@@ -306,7 +304,22 @@ public class DocumentIdRequestHandler
     Map<String, Object> map = buildResponse(siteId, documentId, documents, uploadUrls);
 
     ApiResponseStatus status = isUpdate ? SC_OK : SC_CREATED;
+
     return new ApiRequestHandlerResponse(status, new ApiMapResponse(map));
+  }
+
+  /**
+   * Update Content-Type on {@link DynamicObject} based on {@link ApiGatewayRequestEvent}.
+   * 
+   * @param event {@link ApiGatewayRequestEvent}
+   * @param item {@link DynamicObject}
+   */
+  private void updateContentType(final ApiGatewayRequestEvent event, final DynamicObject item) {
+    String contentType = getContentType(event);
+
+    if (!item.containsKey("contentType") && contentType != null) {
+      item.put("contentType", contentType);
+    }
   }
 
   /**
@@ -333,7 +346,7 @@ public class DocumentIdRequestHandler
 
     S3Service s3 = awsservice.s3Service();
     try (S3Client client = s3.buildClient()) {
-      s3.putObject(client, awsservice.stages3bucket(), key, bytes, null);
+      s3.putObject(client, awsservice.stages3bucket(), key, bytes, item.getString("contentType"));
 
       if (maxDocumentCount != null) {
         awsservice.documentCountService().incrementDocumentCount(siteId);
