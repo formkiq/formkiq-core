377fbc1f0bbcc107556093b621a1e2f68781f0d1
Mike Friesen - FormKiQ <mike@formkiq.com>
2022-07-31 11:56:20 -0500

    Added FkqS3Service

---
+++ fkq-test-utils/src/main/java/com/formkiq/testutils/aws/FkqS3Service.java
@@ -0,0 +1,43 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 - 2020 FormKiQ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.formkiq.testutils.aws;
+
+import com.formkiq.aws.s3.S3ConnectionBuilder;
+import com.formkiq.aws.s3.S3Service;
+import software.amazon.awssdk.regions.Region;
+
+/**
+ * 
+ * {@link S3Service} configured specifically for FormKiQ.
+ *
+ */
+public class FkqS3Service extends S3Service {
+
+  /**
+   * constructor.
+   * 
+   * @param awsProfile {@link String}
+   * @param awsRegion {@link Region}
+   */
+  public FkqS3Service(final String awsProfile, final Region awsRegion) {
+    super(new S3ConnectionBuilder().setCredentials(awsProfile).setRegion(awsRegion));
+  }
+}
