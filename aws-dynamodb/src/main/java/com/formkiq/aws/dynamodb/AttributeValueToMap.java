e4b60928642c3efd769d6a42bfa553eb4791eb06
Mike Friesen - FormKiQ <mike@formkiq.com>
2022-11-02 21:32:24 -0500

    update

---
+++ aws-dynamodb/src/main/java/com/formkiq/aws/dynamodb/AttributeValueToMap.java
@@ -0,0 +1,67 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 - 2020 FormKiQ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.formkiq.aws.dynamodb;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+/**
+ * Convert {@link Map} {@link AttributeValue} to {@link Map}.
+ *
+ */
+public class AttributeValueToMap
+    implements Function<Map<String, AttributeValue>, Map<String, Object>> {
+
+  @Override
+  public Map<String, Object> apply(final Map<String, AttributeValue> map) {
+
+    Map<String, Object> result = new HashMap<>();
+
+    for (Map.Entry<String, AttributeValue> e : map.entrySet()) {
+      String s = e.getValue().s();
+      String n = e.getValue().n();
+      String v = s == null && n != null ? n : s;
+      
+      String key = getKey(e.getKey());
+      result.put(key, v);
+    }
+
+    return result;
+  }
+
+  private String getKey(final String key) {
+    
+    String k = key;
+    if ("inserteddate".equals(key)) {
+      k = "insertedDate";
+    } else if (k.startsWith("fk#")) {
+      final int pos = 3;
+      k = k.substring(pos);
+    }
+
+    return k;
+  }
+}
