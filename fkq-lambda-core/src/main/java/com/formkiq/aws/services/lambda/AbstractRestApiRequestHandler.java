89c6d478ea18e798b0a67f352f0fc1062425f584
Mike Friesen <mfriesen@gmail.com>
2023-05-27 11:26:29 -0500

    update

--- fkq-lambda-core/src/main/java/com/formkiq/aws/services/lambda/AbstractRestApiRequestHandler.java
+++ fkq-lambda-core/src/main/java/com/formkiq/aws/services/lambda/AbstractRestApiRequestHandler.java
@@ -231,24 +231,28 @@ public abstract class AbstractRestApiRequestHandler implements RequestStreamHand
 
     ApiGatewayRequestEvent event = this.gson.fromJson(str, ApiGatewayRequestEvent.class);
 
-    ApiGatewayRequestContext requestContext =
-        event.getRequestContext() != null ? event.getRequestContext()
-            : new ApiGatewayRequestContext();
+    if (event != null) {
+      ApiGatewayRequestContext requestContext =
+          event.getRequestContext() != null ? event.getRequestContext()
+              : new ApiGatewayRequestContext();
 
-    Map<String, Object> identity =
-        requestContext.getIdentity() != null ? requestContext.getIdentity() : Map.of();
+      Map<String, Object> identity =
+          requestContext.getIdentity() != null ? requestContext.getIdentity() : Map.of();
 
-    String s = String.format(
-        "{\"requestId\": \"%s\",\"ip\": \"%s\",\"requestTime\": \"%s\",\"httpMethod\": \"%s\","
-            + "\"routeKey\": \"%s\",\"pathParameters\": %s,"
-            + "\"protocol\": \"%s\",\"user\":\"%s\",\"queryParameters\":%s}",
-        requestContext.getRequestId(), identity.get("sourceIp"), requestContext.getRequestTime(),
-        event.getHttpMethod(), event.getHttpMethod() + " " + event.getResource(),
-        "{" + toStringFromMap(event.getPathParameters()) + "}", requestContext.getProtocol(),
-        ApiGatewayRequestEventUtil.getCallingCognitoUsername(event),
-        "{" + toStringFromMap(event.getQueryStringParameters()) + "}");
+      String s = String.format(
+          "{\"requestId\": \"%s\",\"ip\": \"%s\",\"requestTime\": \"%s\",\"httpMethod\": \"%s\","
+              + "\"routeKey\": \"%s\",\"pathParameters\": %s,"
+              + "\"protocol\": \"%s\",\"user\":\"%s\",\"queryParameters\":%s}",
+          requestContext.getRequestId(), identity.get("sourceIp"), requestContext.getRequestTime(),
+          event.getHttpMethod(), event.getHttpMethod() + " " + event.getResource(),
+          "{" + toStringFromMap(event.getPathParameters()) + "}", requestContext.getProtocol(),
+          ApiGatewayRequestEventUtil.getCallingCognitoUsername(event),
+          "{" + toStringFromMap(event.getQueryStringParameters()) + "}");
 
-    logger.log(s);
+      logger.log(s);
+    } else {
+      logger.log("invalid request event");
+    }
 
     return event;
   }
