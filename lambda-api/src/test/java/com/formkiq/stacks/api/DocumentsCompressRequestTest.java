d8c4b29fd9b6bf60b86c38b302acaf3fee3e37f0
Yan Tsipun <80974736+ytsipun@users.noreply.github.com>
2023-07-04 02:43:18 +0300

    feat: extends the tests

--- lambda-api/src/test/java/com/formkiq/stacks/api/DocumentsCompressRequestTest.java
+++ lambda-api/src/test/java/com/formkiq/stacks/api/DocumentsCompressRequestTest.java
@@ -23,26 +23,57 @@
  */
 package com.formkiq.stacks.api;
 
+import com.formkiq.aws.s3.S3Service;
 import com.formkiq.aws.services.lambda.ApiGatewayRequestEvent;
 import com.formkiq.lambda.apigateway.util.GsonUtil;
 import com.formkiq.testutils.aws.DynamoDbExtension;
 import com.formkiq.testutils.aws.LocalStackExtension;
+import com.formkiq.testutils.aws.TestServices;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.formkiq.testutils.aws.TestServices.STAGE_BUCKET_NAME;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(LocalStackExtension.class)
 @ExtendWith(DynamoDbExtension.class)
 public class DocumentsCompressRequestTest extends AbstractRequestHandler {
+  private Gson gson = null;
+  private S3Service s3 = null;
+
+  @Override
+  @BeforeEach
+  public void before() throws Exception {
+    super.before();
+    this.gson = GsonUtil.getInstance();
+    this.s3 = new S3Service(TestServices.getS3Connection(null));
+  }
+
   @Test
+  @SuppressWarnings("unchecked")
   public void testHandlePostDocumentsCompress() throws Exception {
     ApiGatewayRequestEvent event = toRequestEvent("/request-post-documents-compress.json");
     final String response = handleRequest(event);
 
-    final Map<String, String> responseMap = GsonUtil.getInstance().fromJson(response, Map.class);
-    assertEquals("201.0", String.valueOf(responseMap.get("statusCode")));
+    final Map<String, String> responseMap = this.gson.fromJson(response, Map.class);
+    final String responseStatus = String.valueOf(responseMap.get("statusCode"));
+    assertEquals("201.0", responseStatus);
+    final Map<String, Object> responseBody = this.gson.fromJson(responseMap.get("body"), Map.class);
+    assertTrue(responseBody.containsKey("downloadUrl"));
+    assertTrue(responseBody.containsKey("compressionId"));
+
+    // S3
+    final String compressionId = String.valueOf(responseBody.get("compressionId"));
+    final String zipTaskS3Key = String.format("tempfiles/%s.json", compressionId);
+    final String zipTaskS3File = this.s3.getContentAsString(STAGE_BUCKET_NAME, zipTaskS3Key, null);
+    assertNotNull(zipTaskS3File);
+    final Map<String, Object> s3FileMap = this.gson.fromJson(zipTaskS3File, Map.class);
+    assertTrue(s3FileMap.containsKey("compressionId"));
+    assertTrue(s3FileMap.containsKey("downloadUrl"));
+    assertTrue(s3FileMap.containsKey("documentIds"));
   }
 }
