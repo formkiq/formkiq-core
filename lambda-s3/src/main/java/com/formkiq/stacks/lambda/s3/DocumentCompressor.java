9c0956959bbda6344125bf349020e713b807866d
Yan Tsipun <80974736+ytsipun@users.noreply.github.com>
2023-07-24 01:39:40 +0300

    refactor: used multipart upload only for large files

--- lambda-s3/src/main/java/com/formkiq/stacks/lambda/s3/DocumentCompressor.java
+++ lambda-s3/src/main/java/com/formkiq/stacks/lambda/s3/DocumentCompressor.java
@@ -38,6 +38,7 @@ import com.formkiq.stacks.dynamodb.DocumentServiceImpl;
 import com.formkiq.stacks.dynamodb.DocumentVersionService;
 import com.formkiq.stacks.dynamodb.DocumentVersionServiceExtension;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -48,80 +49,82 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class DocumentCompressor {
-    private final static long UPLOAD_CHUNK_5MB = 5 * 1024 * 1024;
     private final static long MAX_IN_MEMORY_FILE_SIZE_50MB = 50 * 1024 * 1024;
     private final S3Service s3;
     private final DocumentService documentService;
 
-    public DocumentCompressor(final Map<String, String> envVars, final S3ConnectionBuilder s3Builder,
-                              final DynamoDbConnectionBuilder dbBuilder) {
+    public DocumentCompressor(final Map<String, String> envVars, final S3ConnectionBuilder s3Builder, final DynamoDbConnectionBuilder dbBuilder) {
         this.s3 = new S3Service(s3Builder);
-        final String documentsTable = envVars.get("DOCUMENTS_TABLE"); // TODO: const?
+        final String documentsTable = envVars.get("DOCUMENTS_TABLE");
         AwsServiceCache serviceCache = new AwsServiceCache().environment(envVars);
-        AwsServiceCache.register(DynamoDbConnectionBuilder.class,
-                new DynamoDbConnectionBuilderExtension(dbBuilder));
+        AwsServiceCache.register(DynamoDbConnectionBuilder.class, new DynamoDbConnectionBuilderExtension(dbBuilder));
         DocumentVersionServiceExtension dsExtension = new DocumentVersionServiceExtension();
         DocumentVersionService versionService = dsExtension.loadService(serviceCache);
         this.documentService = new DocumentServiceImpl(dbBuilder, documentsTable, versionService);
     }
 
-    public void compressDocuments(final String srcBucket, final String destBucket,
-                                  final String siteId, final String compressionId, final List<String> documentIds)
-            throws Exception {
-        final ArrayList<String> s3ObjectKeys = this.getDocumentObjectKeys(siteId, documentIds);
-        if (documentIds.size() != s3ObjectKeys.size()) {
-            // TODO: Refactor, indicate missing document(s)
+    public void compressDocuments(final String siteId, final String srcBucket, final String destBucket, final String archiveKey, final List<String> documentIds) throws Exception {
+        final Map<String, String> documentIdObjectKeyMap = this.getDocumentObjectKeys(siteId, documentIds);
+        if (documentIds.size() != documentIdObjectKeyMap.size()) {
+            // TODO: log missing docs
+            final List<String> missingDocuments = documentIds.stream().filter(docId -> !documentIdObjectKeyMap.containsKey(docId)).collect(Collectors.toList());
             throw new Exception("Can't find all documents");
         }
 
-        final Map<String, Long> objectKeySizeMap = this.GetObjectKeySizeMap(srcBucket, s3ObjectKeys);
-        final String archiveKey = String.format("/tempfiles/%s/%s.zip", siteId, compressionId);
+        final ArrayList<String> objectKeys = new ArrayList<>(documentIdObjectKeyMap.values());
+        final Map<String, Long> objectKeySizeMap = this.getObjectKeySizeMap(srcBucket, objectKeys);
         this.archiveS3Objects(destBucket, siteId, archiveKey, objectKeySizeMap);
     }
 
-    private ArrayList<String> getDocumentObjectKeys(final String siteId,
-                                                    final List<String> documentIds) {
+    private Map<String, String> getDocumentObjectKeys(final String siteId, final List<String> documentIds) {
         final List<DocumentItem> documents = this.documentService.findDocuments(siteId, documentIds);
-        return documents.stream().map(DocumentItem::getPath)
-                .collect(Collectors.toCollection(ArrayList::new));
+        return documents.stream().collect(Collectors.toMap(DocumentItem::getDocumentId, DocumentItem::getPath));
     }
 
-    private Map<String, Long> GetObjectKeySizeMap(final String bucket, final ArrayList<String> keys) {
+    private Map<String, Long> getObjectKeySizeMap(final String bucket, final ArrayList<String> keys) {
         return keys.stream().collect(Collectors.toMap(String::new, key -> this.s3.getObjectMetadata(bucket, key, null).getContentLength()));
     }
 
-    private void archiveS3Objects(final String bucket, final String siteId, final String archiveKey,
-                                  Map<String, Long> objectKeySizeMap) throws IOException {
-        final MultiPartOutputStream s3OutputStream = this.getS3UploadStream(bucket, archiveKey, null); // TODO: Provide client
-        ZipOutputStream zipOutputStream = new ZipOutputStream(s3OutputStream);
+    private void archiveS3Objects(final String bucket, final String siteId, final String archiveKey, Map<String, Long> objectKeySizeMap) throws IOException {
+        MultiPartOutputStream s3OutputStream;
+        ByteArrayOutputStream byteArrayOutputStream;
+        ZipOutputStream zipOutputStream;
+        final Long totalFilesSize = objectKeySizeMap.values().stream().reduce(0L, Long::sum);
+        final boolean isMultiPartUpload = totalFilesSize > MAX_IN_MEMORY_FILE_SIZE_50MB;
+
+        if (isMultiPartUpload) {
+            // TODO: Provide with S3 client
+            s3OutputStream = this.getS3MultiPartUploadStream(bucket, archiveKey, null);
+            zipOutputStream = new ZipOutputStream(s3OutputStream);
+        } else {
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
+        }
+
         objectKeySizeMap.forEach((objectKey, objectSize) -> {
             final String fileName = Strings.getFilename(objectKey);
             final ZipEntry zipEntry = new ZipEntry(fileName);
 
             try {
                 zipOutputStream.putNextEntry(zipEntry);
-                if (objectSize <= MAX_IN_MEMORY_FILE_SIZE_50MB) {
-                    final InputStream content = this.s3.getContentAsInputStream(bucket, objectKey);
-                    content.transferTo(zipOutputStream);
+                if (isMultiPartUpload) {
+                    this.transferObjectToZipByChunks(zipOutputStream, bucket, objectKey, objectSize);
                 } else {
-                    this.archiveS3ObjectInChunks(zipOutputStream, bucket, objectKey, objectSize);
+                    this.transferObjectToZip(zipOutputStream, bucket, objectKey);
                 }
                 zipOutputStream.closeEntry();
             } catch (IOException e) {
-                s3OutputStream.close();
-                zipOutputStream.close();
+                // TODO: Close streams?
             }
         });
     }
 
-    private MultiPartOutputStream getS3UploadStream(final String bucket, final String key,
-                                                    final AmazonS3 client) {
+    private MultiPartOutputStream getS3MultiPartUploadStream(final String bucket, final String key, final AmazonS3 client) {
         final StreamTransferManager manager = new StreamTransferManager(bucket, key, client);
         return manager.getMultiPartOutputStreams().get(0);
     }
 
-    private void archiveS3ObjectInChunks(final ZipOutputStream outputStream, final String bucket, final String key,
-                                         final Long objectSize) throws IOException {
+    private void transferObjectToZipByChunks(final ZipOutputStream outputStream, final String bucket, final String key, final Long objectSize) throws IOException {
         final long lastByte = objectSize - 1;
         long start = 0L;
         long end = Math.min(start + MAX_IN_MEMORY_FILE_SIZE_50MB, lastByte);
@@ -135,5 +138,10 @@ public class DocumentCompressor {
                 throw e;
             }
         }
+    }
+
+    private void transferObjectToZip(final ZipOutputStream outputStream, final String bucket, final String key) throws IOException {
+        final InputStream content = this.s3.getContentAsInputStream(bucket, key);
+        content.transferTo(outputStream);
     }
 }
