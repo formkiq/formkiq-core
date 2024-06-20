975220b2a4d124830a3917abbef1616e4e1d169d
Mike Friesen <mfriesen@gmail.com>
2024-06-20 08:14:19 -0500

    Updated console to 3.6.0

--- console/src/integration/java/com/formkiq/stacks/console/awstest/AwsResourceTest.java
+++ console/src/integration/java/com/formkiq/stacks/console/awstest/AwsResourceTest.java
@@ -201,7 +201,7 @@ public class AwsResourceTest {
    */
   @Test
   public void testSsmParameters() {
-    assertEquals("v3.5.1",
+    assertEquals("v3.6.0",
         ssmService.getParameterValue("/formkiq/" + appenvironment + "/console/version"));
     assertTrue(ssmService.getParameterValue("/formkiq/" + appenvironment + "/s3/Console")
         .contains(appenvironment + "-console-"));
