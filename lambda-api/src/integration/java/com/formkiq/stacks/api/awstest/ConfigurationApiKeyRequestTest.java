3cee7c665579667df727e8468c5df2b7e0446baf
Mike Friesen <mfriesen@gmail.com>
2024-09-30 15:46:04 -0500

    update

--- lambda-api/src/integration/java/com/formkiq/stacks/api/awstest/ConfigurationApiKeyRequestTest.java
+++ lambda-api/src/integration/java/com/formkiq/stacks/api/awstest/ConfigurationApiKeyRequestTest.java
@@ -32,8 +32,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import com.formkiq.client.model.AddApiKeyRequest;
-import com.formkiq.client.model.AddApiKeyResponse;
+import com.formkiq.client.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -44,9 +43,6 @@ import com.formkiq.client.api.SystemManagementApi;
 import com.formkiq.client.invoker.ApiClient;
 import com.formkiq.client.invoker.ApiException;
 import com.formkiq.client.model.AddApiKeyRequest.PermissionsEnum;
-import com.formkiq.client.model.AddDocumentRequest;
-import com.formkiq.client.model.AddDocumentResponse;
-import com.formkiq.client.model.GetApiKeysResponse;
 import com.formkiq.testutils.aws.AbstractAwsIntegrationTest;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AuthenticationResultType;
 
@@ -208,6 +204,36 @@ public class ConfigurationApiKeyRequestTest extends AbstractAwsIntegrationTest {
 
       // then
       assertNotNull(response.getDocumentId());
+    }
+  }
+
+  /**
+   * Test Add API key.
+   *
+   * @throws Exception Exception
+   */
+  @Test
+  @Timeout(value = TEST_TIMEOUT)
+  public void testAddApiKey01() throws Exception {
+    // given
+    for (String siteId : Arrays.asList("default", UUID.randomUUID().toString())) {
+      ApiClient apiClient = getApiClients(siteId).get(0);
+      SystemManagementApi api = new SystemManagementApi(apiClient);
+
+      AddApiKeyRequest req = new AddApiKeyRequest().name(UUID.randomUUID().toString())
+          .addPermissionsItem(PermissionsEnum.GOVERN);
+
+      // when
+      AddApiKeyResponse response = api.addApiKey(siteId, req);
+
+      // then
+      ApiClient apiClientWithToken = getApiClientWithToken(response.getApiKey());
+      api = new SystemManagementApi(apiClientWithToken);
+      List<Site> sites = notNull(api.getSites().getSites());
+      assertEquals(1, sites.size());
+      assertEquals(siteId, sites.get(0).getSiteId());
+      assertEquals("GOVERN",
+          String.join(",", sites.get(0).getPermissions().stream().map(Enum::name).toList()));
     }
   }
 }
