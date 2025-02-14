95b0ff3dc576c6a7a64d829a718841c6b9c680dc
Mike Friesen <mfriesen@gmail.com>
2025-02-14 16:21:11 -0600

    #261 - Locale Open API

--- fkq-lambda-services/src/main/java/com/formkiq/module/lambdaservices/timer/MethodTimer.java
+++ fkq-lambda-services/src/main/java/com/formkiq/module/lambdaservices/timer/MethodTimer.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 - 2020 FormKiQ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.formkiq.module.lambdaservices.timer;
 
 import com.formkiq.module.lambdaservices.logger.LogLevel;
@@ -8,69 +31,76 @@ import com.formkiq.module.lambdaservices.logger.Logger;
  */
 public class MethodTimer {
 
-    /** Timer Name. */
-    private String timerName;
-    /** Start of Timer. */
-    private long startTime = -1;
-    /** End of Timer. */
-    private long endTime = -1;
-    /**
-     * constructor.
-     */
-    public MethodTimer() {
-    }
+  /** Timer Name. */
+  private String timerName;
+  /** Start of Timer. */
+  private long startTime = -1;
+  /** End of Timer. */
+  private long endTime = -1;
 
-    /**
-     * Start Timer.
-     * @param name {@link String}
-     * @return MethodTimer
-     */
-    public static MethodTimer startTimer(final String name) {
-        return new MethodTimer().start(name);
-    }
+  /**
+   * constructor.
+   */
+  public MethodTimer() {}
 
-    /**
-     * Start Timer.
-     * @param name {@link String}
-     * @return MethodTimer
-     */
-    public MethodTimer start(final String name) {
-        this.timerName = name;
-        this.startTime = System.currentTimeMillis();
-        return this;
-    }
+  /**
+   * Start Timer.
+   * 
+   * @param name {@link String}
+   * @return MethodTimer
+   */
+  public static MethodTimer startTimer(final String name) {
+    return new MethodTimer().start(name);
+  }
 
-    /**
-     * Stop Timer.
-     * @return MethodTimer
-     */
-    public MethodTimer stop() {
-        if (startTime < 0) {
-            throw new IllegalStateException("Timer has not been started.");
-        }
-        endTime = System.currentTimeMillis();
-        return this;
-    }
+  /**
+   * Start Timer.
+   * 
+   * @param name {@link String}
+   * @return MethodTimer
+   */
+  public MethodTimer start(final String name) {
+    this.timerName = name;
+    this.startTime = System.currentTimeMillis();
+    return this;
+  }
 
-    /**
-     * Returns the elapsed time in nanoseconds.
-     * @return elapsed time in nanoseconds.
-     * @throws IllegalStateException if the timer is still running.
-     */
-    public long getElapsedTime() {
-        if (endTime < 0) {
-            throw new IllegalStateException("Timer is still running. Stop it before getting the elapsed time.");
-        }
-        return endTime - startTime;
+  /**
+   * Stop Timer.
+   * 
+   * @return MethodTimer
+   */
+  public MethodTimer stop() {
+    if (startTime < 0) {
+      throw new IllegalStateException("Timer has not been started.");
     }
+    endTime = System.currentTimeMillis();
+    return this;
+  }
 
-    /**
-     * Log the Elapsed Time.
-     * @param logger {@link Logger}
-     */
-    public void logElapsedTime(final Logger logger) {
-        if (logger.isLogged(LogLevel.TRACE)) {
-            logger.trace(String.format("%s: elapsed time in milliseconds: %d", this.timerName, getElapsedTime()));
-        }
+  /**
+   * Returns the elapsed time in nanoseconds.
+   * 
+   * @return elapsed time in nanoseconds.
+   * @throws IllegalStateException if the timer is still running.
+   */
+  public long getElapsedTime() {
+    if (endTime < 0) {
+      throw new IllegalStateException(
+          "Timer is still running. Stop it before getting the elapsed time.");
     }
+    return endTime - startTime;
+  }
+
+  /**
+   * Log the Elapsed Time.
+   * 
+   * @param logger {@link Logger}
+   */
+  public void logElapsedTime(final Logger logger) {
+    if (logger.isLogged(LogLevel.TRACE)) {
+      logger.trace(
+          String.format("%s: elapsed time in milliseconds: %d", this.timerName, getElapsedTime()));
+    }
+  }
 }
