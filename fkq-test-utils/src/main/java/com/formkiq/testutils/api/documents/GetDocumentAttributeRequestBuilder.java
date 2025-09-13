ba9e1513e94671d5c77d6749804db88b40e30086
Mike Friesen <mfriesen@gmail.com>
2025-09-12 21:23:49 -0500

    Added GetDocumentAttributeRequestBuilder

---
+++ fkq-test-utils/src/main/java/com/formkiq/testutils/api/documents/GetDocumentAttributeRequestBuilder.java
@@ -0,0 +1,69 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 - 2020 FormKiQ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.formkiq.testutils.api.documents;
+
+import com.formkiq.client.api.DocumentAttributesApi;
+import com.formkiq.client.invoker.ApiClient;
+import com.formkiq.client.invoker.ApiException;
+import com.formkiq.client.model.GetDocumentAttributeResponse;
+import com.formkiq.testutils.api.ApiHttpResponse;
+import com.formkiq.testutils.api.HttpRequestBuilder;
+
+/**
+ * Builder for Get Document Attribute Request.
+ */
+public class GetDocumentAttributeRequestBuilder implements HttpRequestBuilder {
+
+  /** {@link String}. */
+  private final String docId;
+  /** {@link String}. */
+  private final String key;
+
+  /**
+   * constructor.
+   *
+   * @param documentId {@link String}
+   * @param attributeKey {@link String}
+   */
+  public GetDocumentAttributeRequestBuilder(final String documentId, final String attributeKey) {
+    this.docId = documentId;
+    this.key = attributeKey;
+  }
+
+  @Override
+  public ApiHttpResponse<GetDocumentAttributeResponse> submit(final ApiClient apiClient,
+      final String siteId) {
+    ApiException ex = null;
+    GetDocumentAttributeResponse response = null;
+
+    try {
+      response =
+          new DocumentAttributesApi(apiClient).getDocumentAttribute(this.docId, this.key, siteId);
+    } catch (ApiException e) {
+      ex = e;
+    }
+
+    return new ApiHttpResponse<>(response, ex);
+  }
+}
