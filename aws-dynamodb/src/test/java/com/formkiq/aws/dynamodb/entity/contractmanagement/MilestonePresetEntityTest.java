f0706a6b65f80e2c931fbb7f7481beb7dfa5e482
Mike Friesen <mfriesen@gmail.com>
2026-05-11 21:44:37 -0500

    update

---
+++ aws-dynamodb/src/test/java/com/formkiq/aws/dynamodb/entity/contractmanagement/MilestonePresetEntityTest.java
@@ -0,0 +1,65 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 - 2020 FormKiQ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.formkiq.aws.dynamodb.entity.contractmanagement;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** Unit Tests for {@link MilestonePresetEntity}. */
+class MilestonePresetEntityTest {
+
+  /**
+   * Get Milestone attribute keys.
+   */
+  @Test
+  void testGetAttributeKeys01() {
+    // given
+    MilestonePresetEntity entity = new MilestonePresetEntity();
+
+    // when
+    List<String> keys = entity.getAttributeKeys();
+
+    // then
+    assertEquals(List.of("MilestoneDate", "MilestoneDescription", "MilestoneFeePercentageDue"),
+        keys);
+  }
+
+  /**
+   * Get Milestone name.
+   */
+  @Test
+  void testGetName01() {
+    // given
+    MilestonePresetEntity entity = new MilestonePresetEntity();
+
+    // when
+    String name = entity.getName();
+
+    // then
+    assertEquals("Milestone", name);
+  }
+}
